e2500157d58ab2c1dd655a09822bfb01256d9a03
Rumblekat <reki318@naver.com>
2022-10-03 13:36:31 +0900

    (docs) create docs of function of baseball game

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameManager {
     public static int [] ans = new int [3];
-
+    //TODO: 1. 정답 숫자 생성 (O)
     public static void generateNumber(){
         StringBuilder sb = new StringBuilder();
         int num1 = Randoms.pickNumberInRange(1,9);
@@ -17,6 +17,7 @@ public class GameManager {
         ans[2] = answerNum.charAt(2) - '0';
     }
 
+    //TODO: 2. 중복을 허용하지 않는 숫자 생성 (O)
     private static int getUniqueNumber(String param) {
         int num;
         for(num = Randoms.pickNumberInRange(1,9); !param.contains(String.valueOf(num));){
@@ -25,6 +26,7 @@ public class GameManager {
         return 0;
     }
 
+    //TODO: 3. 사용자 입력 기능 및 예외처리 (O)
     public static int[] getInput() {
         String intputNum = Console.readLine();
         if (intputNum.length() != 3) {
@@ -44,6 +46,7 @@ public class GameManager {
         };
     }
 
+    //TODO: 4. 게임 클리어 후 옵션 (O)
     public static boolean checkNextGame() {
         String inputString = Console.readLine();
         if (inputString.contentEquals("1")) {
@@ -54,10 +57,12 @@ public class GameManager {
         throw new IllegalArgumentException();
     }
 
+    //TODO: 5. 게임 오버 여부 (O)
     public static boolean is3Out(int[] inputNumber) {
         return calcStrike(inputNumber) == 3;
     }
 
+    //TODO: 6. 스트라이크 갯수 계산 기능 (O)
     public static int calcStrike(int [] inputNumber){
         int strikeCount = 0;
         if (ans[0] == inputNumber[0]) strikeCount++;
@@ -66,6 +71,7 @@ public class GameManager {
         return strikeCount;
     }
 
+    //TODO: 7. 볼렛 갯수 계산 기능 (O)
     public static int calcBallet(int [] inputNumber) {
         int ballCount = 0;
         if (ans[1] == inputNumber[0] || ans[2] == inputNumber[0]) ballCount++;
@@ -74,7 +80,7 @@ public class GameManager {
         return ballCount;
     }
 
-
+    //TODO: 8. 결과 산출 기능 (O)
     public static void calcResult(int[] inputNumber) {
         int strikeCount = calcStrike(inputNumber);
         int ballCount = calcBallet(inputNumber);
